57e7feb046f9722b36b172da55e1d4c61b4092e2
muhammad-danish1998 <iamdanish1998@gmail.com>
2020-05-31 12:19:39 +0500

    Updates

---
+++ precache-manifest.69aae5d1fd961d22d5572c33cf936bfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba50db6991c1c51d63a298455b6d97a",
+    "url": "/recipes-app/index.html"
+  },
+  {
+    "revision": "a989d916bd2a75343225",
+    "url": "/recipes-app/static/css/main.e43cf5a0.chunk.css"
+  },
+  {
+    "revision": "d41dcb4bd3989b10f821",
+    "url": "/recipes-app/static/js/2.9cbc5f59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipes-app/static/js/2.9cbc5f59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a989d916bd2a75343225",
+    "url": "/recipes-app/static/js/main.d5ae7ea8.chunk.js"
+  },
+  {
+    "revision": "9f04c846014b88c9d222",
+    "url": "/recipes-app/static/js/runtime-main.7b3c6a76.js"
+  }
+]);
